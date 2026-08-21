f6cae0036942b57287b6f40787ae5b7f73bf168f
Dev VipV1 <huyduc123122@gmail.com>
2026-08-21 22:10:14 +0700

    Fix UI blur on startup and add fullscreen black style

--- app/AppDelegate.m
+++ app/AppDelegate.m
@@ -5,9 +5,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor blackColor];
+    
     ViewController *vc = [[ViewController alloc] init];
     self.window.rootViewController = vc;
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
--- app/ViewController.m
+++ app/ViewController.m
@@ -1,32 +1,53 @@
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
+
+@interface ViewController () <WKNavigationDelegate>
+@property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UIActivityIndicatorView *spinner;
+@end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Cấu hình WebView tối ưu cho phát video
+    self.view.backgroundColor = [UIColor blackColor];
+
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     config.allowsInlineMediaPlayback = YES;
     config.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
-    
+
     self.webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
     self.webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    self.webView.navigationDelegate = self;
-    self.webView.UIDelegate = self;
     self.webView.backgroundColor = [UIColor blackColor];
-    self.webView.scrollView.bounces = NO;
-    
+    self.webView.opaque = NO;
+    self.webView.navigationDelegate = self;
+    self.webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+
     [self.view addSubview:self.webView];
-    
-    // URL trang web video của bạn
+
+    self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
+    self.spinner.color = [UIColor whiteColor];
+    self.spinner.center = self.view.center;
+    self.spinner.hidesWhenStopped = YES;
+    [self.view addSubview:self.spinner];
+    [self.spinner startAnimating];
+
+    // Thay thế bằng link web video của bạn
     NSURL *url = [NSURL URLWithString:@"https://doggyv13.netlify.app/"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
 }
 
-- (BOOL)prefersStatusBarHidden {
-    return NO;
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    [self.spinner stopAnimating];
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    [self.spinner stopAnimating];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
 }
 
 @end
