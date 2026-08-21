7115a0efcc310dc6fa554c1a32779e817190511a
Dev VipV1 <huyduc123122@gmail.com>
2026-08-21 22:22:40 +0700

    Add auth screen with key minhhocgioi and transport security

--- app/ViewController.m
+++ app/ViewController.m
@@ -1,9 +1,13 @@
 #import "ViewController.h"
 #import <WebKit/WebKit.h>
 
-@interface ViewController () <WKNavigationDelegate>
+@interface ViewController () <WKNavigationDelegate, UITextFieldDelegate>
+
 @property (nonatomic, strong) WKWebView *webView;
 @property (nonatomic, strong) UIActivityIndicatorView *spinner;
+@property (nonatomic, strong) UIView *authView;
+@property (nonatomic, strong) UITextField *keyTextField;
+
 @end
 
 @implementation ViewController
@@ -12,6 +16,89 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
 
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    BOOL isUnlocked = [prefs boolForKey:@"is_app_unlocked"];
+
+    if (isUnlocked) {
+        [self setupVideoPlayer];
+    } else {
+        [self setupAuthUI];
+    }
+}
+
+- (void)setupAuthUI {
+    self.authView = [[UIView alloc] initWithFrame:self.view.bounds];
+    self.authView.backgroundColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.12 alpha:1.0];
+    self.authView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:self.authView];
+
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 140, self.view.bounds.size.width - 40, 40)];
+    titleLabel.text = @"XÁC THỰC BẢN QUYỀN";
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.font = [UIFont boldSystemFontOfSize:22];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [self.authView addSubview:titleLabel];
+
+    UILabel *subLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 185, self.view.bounds.size.width - 40, 30)];
+    subLabel.text = @"Vui lòng nhập mã kích hoạt để tiếp tục";
+    subLabel.textColor = [UIColor lightGrayColor];
+    subLabel.font = [UIFont systemFontOfSize:14];
+    subLabel.textAlignment = NSTextAlignmentCenter;
+    subLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [self.authView addSubview:subLabel];
+
+    self.keyTextField = [[UITextField alloc] initWithFrame:CGRectMake(35, 240, self.view.bounds.size.width - 70, 50)];
+    self.keyTextField.placeholder = @"Nhập mã Key...";
+    self.keyTextField.textColor = [UIColor whiteColor];
+    self.keyTextField.backgroundColor = [UIColor colorWithRed:0.18 green:0.18 blue:0.2 alpha:1.0];
+    self.keyTextField.layer.cornerRadius = 10;
+    self.keyTextField.layer.borderWidth = 1;
+    self.keyTextField.layer.borderColor = [UIColor colorWithWhite:0.3 alpha:1.0].CGColor;
+    self.keyTextField.textAlignment = NSTextAlignmentCenter;
+    self.keyTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.keyTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.keyTextField.delegate = self;
+    self.keyTextField.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [self.authView addSubview:self.keyTextField];
+
+    UIButton *submitBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    submitBtn.frame = CGRectMake(35, 310, self.view.bounds.size.width - 70, 50);
+    submitBtn.backgroundColor = [UIColor colorWithRed:0.0 green:0.48 blue:1.0 alpha:1.0];
+    [submitBtn setTitle:@"KÍCH HOẠT" forState:UIControlStateNormal];
+    [submitBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    submitBtn.titleLabel.font = [UIFont boldSystemFontOfSize:16];
+    submitBtn.layer.cornerRadius = 10;
+    submitBtn.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [submitBtn addTarget:self action:@selector(verifyKeyAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.authView addSubview:submitBtn];
+}
+
+- (void)verifyKeyAction {
+    [self.view endEditing:YES];
+    NSString *inputKey = [self.keyTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+    if ([inputKey isEqualToString:@"minhhocgioi"]) {
+        NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+        [prefs setBool:YES forKey:@"is_app_unlocked"];
+        [prefs synchronize];
+
+        [UIView animateWithDuration:0.3 animations:^{
+            self.authView.alpha = 0.0;
+        } completion:^(BOOL finished) {
+            [self.authView removeFromSuperview];
+            [self setupVideoPlayer];
+        }];
+    } else {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Lỗi kích hoạt"
+                                                                       message:@"Mã khoá không chính xác. Vui lòng thử lại!"
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Đóng" style:UIAlertActionStyleCancel handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
+
+- (void)setupVideoPlayer {
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     config.allowsInlineMediaPlayback = YES;
     config.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
@@ -22,7 +109,6 @@
     self.webView.opaque = NO;
     self.webView.navigationDelegate = self;
     self.webView.scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
-
     [self.view addSubview:self.webView];
 
     self.spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
@@ -47,6 +133,11 @@
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self verifyKeyAction];
+    return YES;
 }
 
 @end
